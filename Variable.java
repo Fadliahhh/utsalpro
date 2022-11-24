d385e895375e17982faa819409d9024cfec55478
Fadliahhh <mfadli091132@gmail.com>
2022-11-24 12:35:04 +0700

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,12 @@
+class Variable {
+    public static void main(String[] args) {
+        String nama = "Muhammad Fadli";
+        Integer TahunLahir = 2002;
+
+        System.out.println("Assalamu'alaikum"); 
+        System.out.println("Nama Saya :");
+        System.out.println(nama);
+        System.out.println("Saya Lahir Di Tahun :");
+        System.out.println(TahunLahir);
+    }
+}
